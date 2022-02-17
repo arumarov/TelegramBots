cd252beaf4d0308a25ece61bea36b86c65abd2bc
AntonUmarov <arumarov@yandex.ru>
2022-02-17 19:29:36 +0600

    Add bots

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
-
-
-def hello(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(f'Hello {update.effective_user.first_name}')
-
+from bot_commands import *
 
 updater = Updater('5256574035:AAG5EheEO5-AZxRMGGLksWMYm5gbXfjV9oU')
 
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('hello', hi_command))
+updater.dispatcher.add_handler(CommandHandler('time', time_command))
+updater.dispatcher.add_handler(CommandHandler('help', help_command))
+updater.dispatcher.add_handler(CommandHandler('sum', sum_command))
+
 print('server start')
 updater.start_polling()
 updater.idle()

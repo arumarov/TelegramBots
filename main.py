4bf5b206e6c0fab20c03e8bd57b2ae7d0d9744f1
AntonUmarov <arumarov@yandex.ru>
2022-02-19 22:56:20 +0600

    Correct main

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ from bot_commands import *
 updater = Updater('5256574035:AAG5EheEO5-AZxRMGGLksWMYm5gbXfjV9oU')
 
 updater.dispatcher.add_handler(CommandHandler('hello', hi_command))
-updater.dispatcher.add_handler(CommandHandler('time', time_command))
-updater.dispatcher.add_handler(CommandHandler('help', help_command))
-updater.dispatcher.add_handler(CommandHandler('sum', sum_command))
 
 print('server start')
 updater.start_polling()

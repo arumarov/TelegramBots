959725c834b89bea93fc4ce71133adae80f3eb16
AntonUmarov <arumarov@yandex.ru>
2022-02-19 22:56:48 +0600

    Correct bot_commands

--- bot_commands.py
+++ bot_commands.py
@@ -10,15 +10,3 @@ def help_command(update: Update, context: CallbackContext):
     log(update, context)
     update.message.reply_text(f'/hi\n/time\n/help\n/sum') 
 
-def time_command(update: Update, context: CallbackContext): 
-    log(update, context)
-    update.message.reply_text(f'{datetime.datetime.now().time()}')
-
-def sum_command(update: Update, context: CallbackContext): 
-    log(update, context)
-    msg = update.message.text
-    print(msg)
-    items = msg.split(' ')
-    x = int(items[1])
-    y = int(items[2])
-    update.message.reply_text(f'{x} + {y} = {x+y}')

def123b4043e058c7b4181632d98cbb39cec197f
AntonUmarov <arumarov@yandex.ru>
2022-02-20 21:03:09 +0600

    Create spy.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from bot_commands import *
 
-updater = Updater('5256574035:AAG5EheEO5-AZxRMGGLksWMYm5gbXfjV9oU')
+updater = Updater('5212071316:AAFGsl3UO8zlFGW_4roTLrXZk1VBUL3GNeY')
 
-updater.dispatcher.add_handler(CommandHandler('hello', hi_command))
-
+updater.dispatcher.add_handler(CommandHandler('hi', hi_command))
+updater.dispatcher.add_handler(CommandHandler('time', time_command)) #добавляем сколько угодно комманд
+updater.dispatcher.add_handler(CommandHandler('help', help_command))
+updater.dispatcher.add_handler(CommandHandler('sum', sum_command))
 print('server start')
 updater.start_polling()
 updater.idle()

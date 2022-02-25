bce0769c4f5ae2564a6634b0dc7960d09f73eb0a
AntonUmarov <arumarov@yandex.ru>
2022-02-25 22:44:40 +0600

    Create def game_candy

--- CandyBot/tests.py
+++ CandyBot/tests.py
@@ -9,6 +9,7 @@ import random ###
 # Тестируем вызов функции start_candy (стартовое сообщение)
 # def start_candy():
 #     global sum
+#     global g
 #     g = 2021
 #     sum = (f'Количество конфет на столе: {g}.')
 #     sum1 = (f'\nПервый ход определяется жеребьевкой. \nЗа один ход можно забрать не более 28 конфет. \nВсе конфеты оппонента достаются сделавшему последний ход. \nПроведем жеребьевку? Введите 1 или 2')
@@ -16,18 +17,22 @@ import random ###
 
 # Тестируем вызов функции choice_move (СДЕЛАНА В ОФЛАЙНЕ, АДАПТИРОВАТЬ ПОД ТЕЛЕГРАМ)
 
-def choice_move():
-    choice = random.randint(1, 2)
-    message = 'Жеребьевка! Выберите число: 1 или 2'
-    print(message)
-    mes1 = int(input())
-    if mes1 != 1 and mes1 != 2: res = choice_move()
-    elif mes1 == choice: res = 'Вы ходите первым'
-    elif mes1 != choice: res = 'Компьютер ходит первым'
-    return res
-print(choice_move())
+# def choice_move():
+#     choice = random.randint(1, 2)
+#     message = 'Жеребьевка! Выберите число: 1 или 2'
+#     print(message)
+#     mes1 = int(input())
+#     if mes1 != 1 and mes1 != 2: res = choice_move()
+#     elif mes1 == choice: res = 'Вы ходите первым'
+#     elif mes1 != choice: res = 'Компьютер ходит первым'
+#     return res
+# print(choice_move())
     # update.message.reply_text(f'Жеребьевка {update.effective_user.first_name}!')
 
+# Тестируем вызов функции game_candy
+
+def game_candy():
+    
 
 
 

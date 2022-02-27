934787065df165673363d9e8f01a5d1e70da3bf8
AntonUmarov <arumarov@yandex.ru>
2022-02-27 19:47:53 +0600

    Fill bot_comands

--- CandyBot/bot_commands.py
+++ CandyBot/bot_commands.py
@@ -1,10 +1,31 @@
-from telegram import Update ###
-from telegram.ext import Updater, CommandHandler, CallbackContext ###
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters, ConversationHandler
+import random
 
 
+def hello_command(update: Update, context: CallbackContext):
+    update.message.reply_text(f'Hello {update.effective_user.first_name}!')
 
-updater = Updater('5224472726:AAGUN3lpLqGm2XLbgv7AFacCb-QjWFj0ScU') ###
+def help_command(update: Update, context: CallbackContext): 
+    update.message.reply_text(f'/hello\n/start\n/help\n/choice')
 
+def start_candy(update: Update, context: CallbackContext):
+    global sum
+    global g
+    g = 100
+    sum = (f'Количество конфет на столе: {g}.')
+    sum1 = (f'\nПервый ход определяется жеребьевкой. \nЗа один ход можно забрать не более 28 конфет. \nВсе конфеты оппонента достаются сделавшему последний ход. \nПроведем жеребьевку?')
+    update.message.reply_text(sum + sum1)
+    choice_move()
 
-updater.start_polling()
-updater.idle()
+def choice_move(update: Update, context: CallbackContext):
+    choice = random.randint(1, 2)
+    update.message.reply_text('Жеребьевка! Введите число: 1 или 2: ')
+    mes = update.message.from_user
+    update.message.reply_text(mes)
+    mes1 = int(mes)
+    if mes1 != 1 and mes1 != 2: choice_move
+    elif mes1 == choice: res = 'Вы ходите первым'
+    elif mes1 != choice: res = 'Компьютер ходит первым'
+    update.message.reply_text(res)
+
--- CandyBot/main.py
+++ CandyBot/main.py
@@ -1,3 +1,14 @@
-import controller as cont #####
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters, ConversationHandler
+from bot_commands import *
 
-cont.click_button() #####
+updater = Updater('5224472726:AAGUN3lpLqGm2XLbgv7AFacCb-QjWFj0ScU')
+
+updater.dispatcher.add_handler(CommandHandler('hello', hello_command))
+updater.dispatcher.add_handler(CommandHandler('start', start_candy))
+updater.dispatcher.add_handler(CommandHandler('choice', choice_move))
+updater.dispatcher.add_handler(CommandHandler('help', help_command))
+
+print('server start')
+updater.start_polling()
+updater.idle()
